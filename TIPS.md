00b0d77f92aa55fa5b90660916b4258e236d8047
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@yahoo.com.mx>
2022-05-02 19:09:03 +0200

    5 - Functional Components

--- TIPS.md
+++ TIPS.md
@@ -2,3 +2,10 @@
 ```
 npm i -S @types/ramda ramda --save 
 ```
+
+# Shortcuts to create react component
+```
+rcc->  == class component skeleton
+rsc->  == stateless component skeleton
+rsf->  == stateless named function skeleton
+```
--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
+import Greet from 'components/greet/Greet';
 import './App.css';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet/>
     </div>
   );
 }
